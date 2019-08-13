d166621f7b8035ffb380d0ce396f7c86c45bca48
X3ZvaWQ <x3zvawq@gmail.com>
2019-08-13 22:58:18 +0800

    fix : CORS middleware

--- app/Http/Middleware/EnableCrossRequestMiddleware.php
+++ app/Http/Middleware/EnableCrossRequestMiddleware.php
@@ -16,9 +16,9 @@ class EnableCrossRequestMiddleware
     public function handle($request, Closure $next)
     {
         header('Access-Control-Allow-Origin: *');
-        header("Access-Control-Allow-Credentials: true");
+        header("Access-Control-Allow-Credentials: false");
         header("Access-Control-Allow-Methods: *");
-        header("Access-Control-Allow-Headers: Content-Type,Access-Token");
+        header("Access-Control-Allow-Headers: *");
         header("Access-Control-Expose-Headers: *");
 
         return $next($request);
